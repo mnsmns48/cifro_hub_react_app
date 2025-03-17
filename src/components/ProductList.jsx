685c52462a8481a995469d46f77556cb39d065b8
mnsmns48 <getzboy@list.ru>
2025-03-17 20:19:18 +0300

    add pagination

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,8 +1,9 @@
-import {Card, Image} from "antd";
+import {Card, Image, Pagination} from "antd";
 import Meta from "antd/es/card/Meta";
 import './ProductList.css';
 import ProductFeatures from "./ProductFeatures.jsx";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams, useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
 
 function cleanTitle(title) {
@@ -11,18 +12,29 @@ function cleanTitle(title) {
 
 const ProductList = ({content, endpoint}) => {
     const navigate = useNavigate();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const location = useLocation();
+    const pageSize = 12;
+    const currentPage = parseInt(searchParams.get('page')) || 1;
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    const currentProducts = Array.isArray(content)  ? content.slice(startIndex, endIndex)  : [];
+
     const handleProductClick = (productId, endpoint) => {
         navigate(`/${endpoint}_product/${productId}`);
     };
+    const handlePageChange = (page) => {
+        setSearchParams({ ...Object.fromEntries(searchParams), page: page });
+    };
 
     return (
-        <>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <div className="product-list-container">
                 {Array.isArray(content) ? (
-                    content.map((item) => (
+                    currentProducts.map((item) => (
                         <Card
                             key={item.code}
-                                // onClick={() => handleProductClick(item.code, endpoint)}
+                            // onClick={() => handleProductClick(item.code, endpoint)}
                             className="product-card"
                             hoverable={true}
                             size={"default"}
@@ -46,7 +58,23 @@ const ProductList = ({content, endpoint}) => {
                     <p>Здесь пусто</p>
                 )}
             </div>
-        </>
+            {Array.isArray(content) && content.length > pageSize && (
+                <div style={{ 
+                    display: 'flex',
+                    justifyContent: 'center',
+                    width: '100%',
+                    padding: '20px 0'
+                }}>
+                    <Pagination
+                        current={currentPage}
+                        total={content.length}
+                        pageSize={pageSize}
+                        onChange={handlePageChange}
+                        showSizeChanger={false}
+                    />
+                </div>
+            )}
+        </div>
     );
 };
 
