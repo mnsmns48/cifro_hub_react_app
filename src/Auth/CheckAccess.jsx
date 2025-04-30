c1e3143ff8400af1d67706f4cfbca502d22f8b7a
mnsmns48 <getzboy@list.ru>
2025-04-30 09:38:14 +0300

    add Authorization

--- src/Auth/CheckAccess.jsx
+++ src/Auth/CheckAccess.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import axios from 'axios';
 import Authorization from "./Authorization.jsx";
 import ServiceApp from "../ServiceApp.jsx";
