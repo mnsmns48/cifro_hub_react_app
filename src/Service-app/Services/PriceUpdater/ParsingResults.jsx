8bcf9f89e5d1f90f1cc4458cde9679821c882c7d
mnsmns48 <getzboy@list.ru>
2025-07-05 23:38:12 +0300

    fix reactor

--- src/Service-app/Services/PriceUpdater/ParsingResults.jsx
+++ src/Service-app/Services/PriceUpdater/ParsingResults.jsx
@@ -1,14 +1,25 @@
-import { useState, useEffect, useMemo, useCallback } from "react";
-import { Table, Button, Input, Select, Spin, message } from "antd";
+import {useState, useMemo, useCallback} from "react";
+import {Table, Button, Input, Select, message} from "antd";
 import "../Css/ParsingResults.css";
-import { createParsingColumns } from "./ParsingResultsColumns.jsx";
-import { deleteParsingItems, reCalcOutputPrices } from "./api.js";
+import {createParsingColumns} from "./ParsingResultsColumns.jsx";
+import {deleteParsingItems} from "./api.js";
 import UploadImagesModal from "./UploadImagesModal.jsx";
-import { fetchRangeRewardsProfiles } from "../RewardRangeSettings/api.js";
 
-const { Search } = Input;
+const {Search} = Input;
 
-let didInitParsingResults = false;
+
+const FilterButton = ({onClick, bg}) => {
+    return (
+        <Button onClick={onClick} style={{
+            backgroundColor: bg,
+            borderRadius: "25%",
+            width: 30,
+            height: 30,
+        }}
+        />
+    );
+}
+
 
 const formatDate = isoString => {
     const date = new Date(isoString);
@@ -16,10 +27,9 @@ const formatDate = isoString => {
     return date.toISOString().slice(0, 16).replace("T", " ");
 };
 
-const ParsingResults = ({ result }) => {
-    console.log(">>> Проп result в ParsingResults:", result);
+const ParsingResults = ({result}) => {
+    console.log(">>>------->>>> Проп result в ParsingResults:", result);
 
-    const [localResult, setLocalResult] = useState(result);
     const [rows, setRows] = useState(result.data ?? []);
     const [selectedRowKeys, setSelected] = useState([]);
     const [expandedRows, setExpanded] = useState(null);
@@ -30,33 +40,6 @@ const ParsingResults = ({ result }) => {
 
     const [uploadModalOpen, setUploadModalOpen] = useState(false);
     const [currentOrigin, setCurrentOrigin] = useState(null);
-
-    const [rewardProfiles, setRewardProfiles] = useState([]);
-    const [selectedProfileId, setSelectedProfileId] = useState(
-        result.range_reward.id
-    );
-    const [isLoading, setIsLoading] = useState(false);
-
-    useEffect(() => {
-        if (!didInitParsingResults && result?.vsl_id) {
-            setLocalResult(result);
-            setRows(result.data ?? []);
-            setSelectedProfileId(result.range_reward?.id);
-            didInitParsingResults = true;
-        }
-    }, [result]);
-
-    useEffect(() => {
-        const loadProfiles = async () => {
-            try {
-                const profiles = await fetchRangeRewardsProfiles();
-                setRewardProfiles(profiles);
-            } catch {
-                message.error("Не удалось загрузить профили");
-            }
-        };
-        loadProfiles();
-    }, []);
 
     const showAllItems = () => setActiveFilter("all");
     const showNoPreviewItems = () => setActiveFilter("noPreview");
@@ -113,26 +96,6 @@ const ParsingResults = ({ result }) => {
         [setRows, showInputPrice, expandedRows, toggleExpand]
     );
 
-    const handleRRProfileChange = async value => {
-        setSelectedProfileId(value);
-        // setIsLoading(true);
-        console.log("Выбран профиль:", value);
-        console.log("VSL ID:", localResult?.vsl_id);
-        if (!localResult?.vsl_id || !value) {
-            console.warn("Отсутствуют данные для перерасчёта");
-            return;
-        }
-        try {
-            const updated = await reCalcOutputPrices(localResult.vsl_id, value);
-            setLocalResult(updated);
-            setRows(updated.data);
-        } catch {
-            message.error("Перерасчёт не удался");
-        } //finally {
-        //     setIsLoading(false);
-        // }
-    };
-
     const hasSelection = selectedRowKeys.length > 0;
 
     return (
@@ -140,90 +103,39 @@ const ParsingResults = ({ result }) => {
             <div>
                 <p>
                     <strong>Категория:</strong>{" "}
-                    {localResult.category.map((c, i) => (
+                    {result.category.map((c, i) => (
                         <span key={i}>{c} </span>
                     ))}
                 </p>
                 <p>
-                    <strong>Дата и время:</strong> {formatDate(localResult.datestamp)}
+                    <strong>Дата и время:</strong> {formatDate(result.datestamp)}
                 </p>
             </div>
 
-            <div
-                style={{
-                    display: "flex",
-                    alignItems: "center",
-                    flexWrap: "wrap",
-                    gap: 15,
-                    padding: "15px 0",
-                }}
-            >
-                <Button onClick={() => setShow(!showInputPrice)}>
-                    {showInputPrice ? "Off" : "₽"}
-                </Button>
-                <Button
-                    onClick={showAllItems}
-                    style={{
-                        backgroundColor: "yellowgreen",
-                        borderRadius: "25%",
-                        width: 30,
-                        height: 30,
-                    }}
-                />
-                <Button
-                    onClick={showNoPreviewItems}
-                    style={{
-                        backgroundColor: "yellow",
-                        borderRadius: "25%",
-                        width: 30,
-                        height: 30,
-                    }}
-                />
-                <Button
-                    onClick={showNoFeaturesItems}
-                    style={{
-                        backgroundColor: "red",
-                        borderRadius: "25%",
-                        width: 30,
-                        height: 30,
-                    }}
-                />
-                <Search
-                    placeholder="Пиши что ищешь"
-                    allowClear
-                    style={{ maxWidth: 500 }}
-                    value={searchText}
-                    onChange={e => setSearch(e.target.value)}
-                />
-                <Spin spinning={isLoading}>
-                    <Select
-                        disabled={isLoading}
-                        placeholder="Профиль вознаграждения"
-                        style={{ minWidth: 200 }}
-                        value={selectedProfileId}
-                        onChange={handleRRProfileChange}
-                    >
-                        {rewardProfiles.map(p => (
-                            <Select.Option key={p.id} value={p.id}>
-                                {p.title}
-                            </Select.Option>
-                        ))}
-                    </Select>
-                </Spin>
+            <div style={{
+                display: "flex",
+                alignItems: "center",
+                flexWrap: "wrap",
+                gap: 15,
+                padding: "15px 0",
+            }}>
+                <Button onClick={() => setShow(!showInputPrice)}>{showInputPrice ? "Off" : "₽"}</Button>
+                <FilterButton onClick={showAllItems} bg="yellowgreen"/>
+                <FilterButton onClick={showNoPreviewItems} bg="yellow"/>
+                <FilterButton onClick={showNoFeaturesItems} bg="red"/>
+                <Search placeholder="Пиши что ищешь" allowClear style={{maxWidth: 500}}
+                        value={searchText}
+                        onChange={e => setSearch(e.target.value)}/>
+                <Select placeholder="Профиль вознаграждения" style={{minWidth: 200}}></Select>
             </div>
 
             {hasSelection && (
-                <Button
-                    danger
-                    style={{ marginBottom: 10, marginLeft: 10 }}
-                    onClick={handleDelete}
-                >
+                <Button danger style={{marginBottom: 10, marginLeft: 10}} onClick={handleDelete}>
                     Удалить ({selectedRowKeys.length})
                 </Button>
             )}
 
             <Table
-                loading={isLoading}
                 rowClassName={rec => {
                     const noFeatures =
                         Array.isArray(rec.features_title) && rec.features_title.length === 0;
