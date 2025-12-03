6da8aa2390e426746a1960d52d576af3e53f8336
mnsmns48 <getzboy@list.ru>
2025-12-03 08:57:19 +0300

    update front

--- src/miniApp-V1/sdk/component/Features/SmartPhone.jsx
+++ src/miniApp-V1/sdk/component/Features/SmartPhone.jsx
@@ -1,5 +1,13 @@
 import nanoreview from './description_sources/nanoreview.js'
 import gsmarena from './description_sources/gsmarena.js';
+import {
+    CalculatorOutlined,
+    CameraOutlined,
+    ClockCircleOutlined, DeploymentUnitOutlined,
+    FullscreenOutlined,
+    FundProjectionScreenOutlined, PlusCircleOutlined, SettingOutlined,
+    ThunderboltOutlined, VideoCameraAddOutlined
+} from "@ant-design/icons";
 
 const sourcesMap = {nanoreview, gsmarena};
 
@@ -13,6 +21,7 @@ function shortSmartPhoneSpecification(infoArray, source) {
 
     if (shortSpecs.month || shortSpecs.year) {
         result.push({
+            icon: <ClockCircleOutlined />,
             title: "Дата выхода",
             specs: [
                 {value: shortSpecs.month},
@@ -23,6 +32,7 @@ function shortSmartPhoneSpecification(infoArray, source) {
 
     if (shortSpecs.displayType || shortSpecs.displaySize) {
         result.push({
+            icon: <FundProjectionScreenOutlined />,
             title: "Дисплей",
             specs: [
                 {value: shortSpecs.displayType},
@@ -33,6 +43,7 @@ function shortSmartPhoneSpecification(infoArray, source) {
 
     if (shortSpecs.displayResolution || shortSpecs.displayRefreshRate) {
         result.push({
+            icon: <FullscreenOutlined />,
             title: "Картинка",
             specs: [
                 {value: shortSpecs.displayResolution, label: " x"},
@@ -43,6 +54,7 @@ function shortSmartPhoneSpecification(infoArray, source) {
 
     if (shortSpecs.batteryCapacity || shortSpecs.batteryMaxPowerCharge) {
         result.push({
+            icon: <PlusCircleOutlined />,
             title: "Батарея",
             specs: [
                 {value: shortSpecs.batteryCapacity, label: " мАч"},
@@ -53,11 +65,13 @@ function shortSmartPhoneSpecification(infoArray, source) {
 
     if (shortSpecs.quickCharge) {
         result.push({
+            icon: <ThunderboltOutlined />,
             title: "Быстрая зарядка",
             specs: [{value: shortSpecs.quickCharge}]
         });
     } else if (shortSpecs.selfieSpecs) {
         result.push({
+            icon: <VideoCameraAddOutlined />,
             title: "Селфи",
             specs: [{value: shortSpecs.selfieSpecs, label: " mpx"}]
         });
@@ -65,6 +79,7 @@ function shortSmartPhoneSpecification(infoArray, source) {
 
     if (shortSpecs.cameraSpecs) {
         result.push({
+            icon: <CameraOutlined />,
             title: "Камеры",
             specs: [{value: shortSpecs.cameraSpecs, label: " мpx"}]
         });
@@ -72,6 +87,7 @@ function shortSmartPhoneSpecification(infoArray, source) {
 
     if (shortSpecs.cpu || shortSpecs.cpuMaxClock || shortSpecs.cpuLithographyProcess) {
         result.push({
+            icon: <CalculatorOutlined />,
             title: "Процессор",
             specs: [
                 {value: shortSpecs.cpu},
@@ -83,6 +99,7 @@ function shortSmartPhoneSpecification(infoArray, source) {
 
     if (shortSpecs.antutuScore) {
         result.push({
+            icon: <SettingOutlined />,
             title: "Тест скорости Antutu",
             specs: [
                 {value: shortSpecs.version ? `v${shortSpecs.version}` : null},
@@ -91,6 +108,7 @@ function shortSmartPhoneSpecification(infoArray, source) {
         });
     } else if (shortSpecs.system) {
         result.push({
+            icon: <DeploymentUnitOutlined />,
             title: "Система",
             specs: [{value: shortSpecs.system}]
         });
@@ -109,13 +127,21 @@ export default function SmartPhone({ info }) {
     return (
         <div style={{ padding: "8px" }}>
             <ul style={{ listStyle: "none", padding: 0, margin: 0, color: "#3a3a3a" }}>
-                {blocks.map(({ title, specs }) => (
-                    <li key={title} style={{ marginBottom: "6px" }}>
-                        <strong>{title}:</strong>{" "}
-                        {specs
-                            .map(s => (s?.value ? `${s.value}${s.label || ""}` : null))
-                            .filter(Boolean)
-                            .join(" ")}
+                {blocks.map(({ icon, title, specs }) => (
+                    <li key={title} style={{margin: "10px"}}>
+                        <strong><span style={{
+                            color: '#e2fc2a',
+                            background: '#3a3a3a',
+                            padding: '6px',
+                            borderRadius: '12px',
+                        }}>{icon}</span> {title}</strong>{" "}
+                        <div style={{display: "inline", lineHeight: 1.9}}>
+                            {specs
+                                .map((s) => (s?.value ? `${s.value}${s.label || ""}` : null))
+                                .filter(Boolean)
+                                .join(" ")}
+                        </div>
+
                     </li>
                 ))}
             </ul>
