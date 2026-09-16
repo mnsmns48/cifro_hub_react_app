0b61c70f380823c1b567008d13529bf7c709624d
mnsmns48 <getzboy@list.ru>
2026-09-16 10:17:43 +0300

    add new logo

--- src/Auth/Authorization.jsx
+++ src/Auth/Authorization.jsx
@@ -1,24 +1,31 @@
 import './Auth.css';
-import {Modal, Button} from "antd";
+import { Modal, Button } from "antd";
 import axios from "axios";
-import {useState} from "react";
-import logo from '/admin-logo2.svg'
+import { useState } from "react";
+import logo from './admin-logo2.svg';
 
 export default function Authorization() {
     const [isModalVisible, setIsModalVisible] = useState(false);
     const [modalContent, setModalContent] = useState("");
+
     const handleSubmit = async (event) => {
         event.preventDefault();
+
         const formData = new FormData(event.target);
         const username = formData.get('username');
         const password = formData.get('password');
+
         try {
             const params = new URLSearchParams();
             params.append('username', username);
             params.append('password', password);
+
             const response = await axios.post('/login', params, {
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
             });
+
             if (response.status === 200 || response.status === 204) {
                 window.location.reload();
             }
@@ -27,7 +34,7 @@ export default function Authorization() {
             setIsModalVisible(true);
             event.target.reset();
         }
-    }
+    };
 
     const handleOk = () => {
         setIsModalVisible(false);
@@ -37,18 +44,44 @@ export default function Authorization() {
         <div className="wrapper">
             <div className="form-container">
                 <div className="logo-container">
-                    <img src={logo} alt="Logo" className="service-logo"/>
+                    <img
+                        src={logo}
+                        alt="Logo"
+                        className="service-logo"
+                    />
                 </div>
+
                 <form onSubmit={handleSubmit}>
                     <label htmlFor="username">Логин</label>
-                    <input type="text" id="username" name="username"/><br/>
+                    <input type="text" id="username" name="username" />
+                    <br />
+
                     <label htmlFor="password">Пароль</label>
-                    <input type="password" id="password" name="password"/><br/>
-                    <input type="submit" value="Войти" className="button"/>
+                    <input type="password" id="password" name="password" />
+                    <br />
+
+                    <input
+                        type="submit"
+                        value="Войти"
+                        className="button"
+                    />
                 </form>
             </div>
-            <Modal title="Ошибка" open={isModalVisible} centered={true}
-                   footer={[<Button key="ok" type="primary" onClick={handleOk}>Закрыть</Button>]}>
+
+            <Modal
+                title="Ошибка"
+                open={isModalVisible}
+                centered={true}
+                footer={[
+                    <Button
+                        key="ok"
+                        type="primary"
+                        onClick={handleOk}
+                    >
+                        Закрыть
+                    </Button>
+                ]}
+            >
                 <p>{modalContent}</p>
             </Modal>
         </div>
